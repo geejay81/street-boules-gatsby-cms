7e613981353c028ffbdb20bbb3afa42f0b2ac0f3
Greg Parr <greg.parr@blueyonder.co.uk>
2023-08-20 21:23:15 +0100

    Added favicons and updated logos at top and bottom

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
 import github from "../img/github-icon.svg";
-import logo from "../img/logo.svg";
+import logo from "../img/word-logo-no-border.png";
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -42,7 +42,8 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+              {/* <img src={logo} alt="Kaldi" style={{ width: "88px" }} /> */}
+              <img src={logo} alt="Kaldi" />
             </Link>
             {/* Hamburger menu */}
             <div
