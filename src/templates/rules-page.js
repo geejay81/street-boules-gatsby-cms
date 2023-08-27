65c1d5aeae38de4a988519e9ef850573b4ee2d19
Greg Parr <greg.parr@blueyonder.co.uk>
2023-08-27 16:26:36 +0100

    correctly placed HeroHeader on Rules page

--- src/templates/rules-page.js
+++ src/templates/rules-page.js
@@ -10,18 +10,20 @@ export const RulesPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content;
 
   return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <HeaderHero title={title} />
-              <PageContent className="content" content={content} />
+    <React.Fragment>
+      <HeaderHero title={title} />
+      <section className="section section--gradient">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-10 is-offset-1">
+              <div className="section">
+                <PageContent className="content" content={content} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </React.Fragment>  
   );
 };
 
