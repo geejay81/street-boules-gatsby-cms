fbbe7084a05a498f5572d1e56a1507302011c402
Greg Parr <greg.parr@blueyonder.co.uk>
2023-09-03 22:28:24 +0100

    updated blog roll layout to bulma cards

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -12,16 +12,11 @@ class BlogRollTemplate extends React.Component {
       <div className="columns is-multiline">
         {posts &&
           posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? 'is-warning' : ''
-                }`}
-              >
-                <header>
-                  {post.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <PreviewCompatibleImage
+
+            <div className="column is-parent is-4 key={post.id}">
+              <article className="card blog-list-item tile is-child">
+                <div className="card-image">
+                <PreviewCompatibleImage
                         imageInfo={{
                           image: post.frontmatter.featuredimage,
                           alt: `featured image thumbnail for post ${post.frontmatter.title}`,
@@ -33,31 +28,71 @@ class BlogRollTemplate extends React.Component {
                               .gatsbyImageData.height,
                         }}
                       />
-                    </div>
-                  ) : null}
-                  <p className="post-meta">
-                    <Link
-                      className="title has-text-primary is-size-4"
-                      to={post.fields.slug}
-                    >
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &nbsp; </span>
-                    <span className="subtitle is-size-5 is-block">
+                </div>
+                <div className="card-content">
+                  <div className="content">
+                    <h3 className='title'><Link
+                          className="has-text-primary is-size-4"
+                          to={post.fields.slug}>
+                          {post.frontmatter.title}
+                        </Link></h3>
+                    <div className="subtitle is-size-5 is-block">
                       {post.frontmatter.date}
-                    </span>
-                  </p>
-                </header>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button is-danger is-outline" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
+                    </div>
+                    <p>
+                      {post.excerpt}
+                    </p>
+                    <Link className="button is-danger is-outline" to={post.fields.slug}>
+                      Keep Reading →
+                    </Link>
+                  </div>
+                </div>
               </article>
             </div>
+
+            // <div className="is-parent column is-6" key={post.id}>
+            //   <article
+            //     className={`blog-list-item tile is-child box notification ${
+            //       post.frontmatter.featuredpost ? 'is-warning' : ''
+            //     }`}
+            //   >
+            //     <header>
+            //       {post.frontmatter.featuredimage ? (
+            //         <div className="featured-thumbnail">
+            //           <PreviewCompatibleImage
+            //             imageInfo={{
+            //               image: post.frontmatter.featuredimage,
+            //               alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+            //               width:
+            //                 post.frontmatter.featuredimage.childImageSharp
+            //                   .gatsbyImageData.width,
+            //               height:
+            //                 post.frontmatter.featuredimage.childImageSharp
+            //                   .gatsbyImageData.height,
+            //             }}
+            //           />
+            //         </div>
+            //       ) : null}
+            //       <p className="post-meta">
+            //         <h3 className='title'><Link
+            //           className="has-text-primary is-size-4"
+            //           to={post.fields.slug}
+            //         >
+            //           {post.frontmatter.title}
+            //         </Link></h3>
+            //         <div className="subtitle is-size-5 is-block">
+            //           {post.frontmatter.date}
+            //         </div>
+            //       </p>
+            //     </header>
+            //     <p>
+            //       {post.excerpt}
+            //     </p>
+            //     <Link className="button is-danger is-outline" to={post.fields.slug}>
+            //       Keep Reading →
+            //     </Link>
+            //   </article>
+            // </div>
           ))}
       </div>
     )
@@ -97,7 +132,7 @@ export default function BlogRoll() {
                   featuredimage {
                     childImageSharp {
                       gatsbyImageData(
-                        width: 120
+                        width: 560
                         quality: 100
                         layout: CONSTRAINED
                       )
