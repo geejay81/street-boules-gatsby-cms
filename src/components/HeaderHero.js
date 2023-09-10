b011350349863e425ed52c4862176036a0f106c7
Greg Parr <greg.parr@blueyonder.co.uk>
2023-09-10 13:45:41 +0100

    added some animation to hero header content and logo on home page

--- src/components/HeaderHero.js
+++ src/components/HeaderHero.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import 'animate.css';
 
 export default function HeaderHero(props) {
 
@@ -8,12 +9,12 @@ export default function HeaderHero(props) {
             <div className="hero-body">
                 <div className="container">
                     { props.title && props.title.length > 0 &&
-                        <h1 className="title">
+                        <h1 className="title animate__animated animate__bounceInLeft">
                             { props.title }
                         </h1>
                     } 
                     { props.subheading && props.subheading.length > 0 &&
-                        <p className="subtitle">
+                        <p className="subtitle animate__animated animate__bounceInLeft">
                             { props.subheading }
                         </p>
                     }
